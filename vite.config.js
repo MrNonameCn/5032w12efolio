b961c784520806e6aff245a4dd270256fb0b80ee
xuzihang <xuzihangpq3@gmail.com>
2025-10-19 18:47:12 +1100

    for week 12 efolio

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+const isCI = process.env.GITHUB_ACTIONS === 'true'
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: isCI ? '5032w12efolio' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
