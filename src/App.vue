4defe92535f1d1d49e1920bbbfe295f600d199d7
xuzihang <xuzihangpq3@gmail.com>
2025-10-19 18:49:04 +1100

    for week 12 efolio

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="This page is prepared for week 12 efolio!" />
+      <HelloWorld msg="This page is prepared for week 12 efolio!!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
