5dc23df5fc0f65105c4f96892f009c2d84e5f57c
xuzihang <xuzihangpq3@gmail.com>
2025-10-19 19:02:33 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: isCI ? '5032w12efolio' : '/',
+  base: isCI ? '/5032w12efolio/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
